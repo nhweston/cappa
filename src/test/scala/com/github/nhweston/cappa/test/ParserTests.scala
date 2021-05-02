70391cfa018a15f47075ebd6878ac37a8566f1ea
nhweston <nhweston1997@gmail.com>
2021-05-02 22:12:18 +1000

    feat: add more parser tests

--- src/test/scala/com/github/nhweston/cappa/test/ParserTests.scala
+++ src/test/scala/com/github/nhweston/cappa/test/ParserTests.scala
@@ -37,6 +37,11 @@ class ParserTests extends AnyWordSpecLike with should.Matchers with EitherValues
       parser.parseAll(parser.name, name) shouldBe a[parser.Success[_]]
     }
 
+    def testError(name: String): Unit = {
+      val parser = new Parser
+      parser.parseAll(parser.name, name) shouldBe a[parser.NoSuccess]
+    }
+
     "match a letter" in {
       test("x")
     }
@@ -55,6 +60,16 @@ class ParserTests extends AnyWordSpecLike with should.Matchers with EitherValues
 
     "match a longer name" in {
       test("__Foo_Bar_123")
+    }
+
+    "not match strings with non-word characters" in {
+      testError("a-b")
+      testError("x y")
+      testError("!@#$%*()")
+    }
+
+    "not match the empty string" in {
+      testError("")
     }
 
   }
@@ -78,16 +93,44 @@ class ParserTests extends AnyWordSpecLike with should.Matchers with EitherValues
       test("a b", App(Var("a"), Var("b")))
     }
 
+    "parse multiple applications" in {
+      test("a b c", App(App(Var("a"), Var("b")), Var("c")))
+    }
+
+    "parse multiple applications with parentheses overriding precedence" in {
+      test("a (b c)", App(Var("a"), App(Var("b"), Var("c"))))
+    }
+
     "parse an abstraction" in {
       test("a => b", Abs("a", Var("b")))
+    }
+
+    "parse chained abstractions (unsugared)" in {
+      test("a => b => c", Abs("a", Abs("b", Var("c"))))
+    }
+
+    "parse chained abstractions (sugared)" in {
+      test("a b => c", Abs("a", Abs("b", Var("c"))))
     }
 
     "parse a let" in {
       test("a = b; c", App(Abs("a", Var("c")), Var("b")))
     }
 
-    "parse multiple applications" in {
-      test("a b c", App(App(Var("a"), Var("b")), Var("c")))
+    "parse seqential lets" in {
+      test("a = b; c = d; e", App(Abs("a", App(Abs("c", Var("e")), Var("d"))), Var("b")))
+    }
+
+    "parse nested lets" in {
+      test("a = b = c; d; e", App(Abs("a", Var("e")), App(Abs("b", Var("d")), Var("c"))))
+    }
+
+    "parse a parameterised let" in {
+      test("a b = c; d", App(Abs("a", Var("d")), Abs("b", Var("c"))))
+    }
+
+    "parse a parameterised let with multiple arguments" in {
+      test("a b c = d; e", App(Abs("a", Var("e")), Abs("b", Abs("c", Var("d")))))
     }
 
   }
